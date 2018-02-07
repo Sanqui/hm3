0a2343a36d11fac725826cf227a470f8f061da51
Sanqui <gsanky@gmail.com>
2018-02-07 15:20:19 +0100

    Add the listing of blank banks

--- main.asm
+++ main.asm
@@ -3,3 +3,32 @@ INCLUDE "constants.asm"
 INCLUDE "src/text.asm"
 INCLUDE "data/text_pointers.asm"
 INCLUDE "build/text.asm"
+
+blankbank: MACRO
+SECTION "Blank bank \1", ROMX[$4000], BANK[\1]
+    db \1 ; BANK(@) doesn't work atm
+    ds $3fff
+ENDM
+
+
+    blankbank $4e
+    blankbank $4f
+    blankbank $51
+    blankbank $52
+    blankbank $53
+    blankbank $54
+    blankbank $55
+    blankbank $56
+    blankbank $57
+    blankbank $58
+    blankbank $59
+    blankbank $5a
+    blankbank $5b
+    blankbank $5c
+    blankbank $5d
+    blankbank $5e
+    blankbank $5f
+    blankbank $62
+    blankbank $63
+    blankbank $64
+    blankbank $65
